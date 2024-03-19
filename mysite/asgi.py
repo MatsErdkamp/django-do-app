5ec25d528b9b07dde611c15a54397ab36914ee29
Mats <m.w.g.erdkamp@student.tue.nl>
2024-03-19 15:43:10 +0100

    change url

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
-
+from mysite.wsgi import *
 
 django_asgi_app = get_asgi_application()
 
@@ -19,7 +19,7 @@ application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
-                path("counter/", CounterConsumer.as_asgi()),
+                path("ws/counter/", CounterConsumer.as_asgi()),
 
             ])
         )
