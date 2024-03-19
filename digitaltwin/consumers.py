a4a7eb11086a75d328d1fef0c8e6e44061bf9269
Mats <m.w.g.erdkamp@student.tue.nl>
2024-03-19 15:51:55 +0100

    websocket group

--- digitaltwin/consumers.py
+++ digitaltwin/consumers.py
@@ -1,18 +1,27 @@
-# consumers.py
-from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import Counter
-from asgiref.sync import sync_to_async
 import json
-
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 
 class CounterConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.counter, _ = await sync_to_async(Counter.objects.get_or_create, thread_sensitive=True)(id=1)
+        self.room_group_name = 'counter_updates'  # Name of the group for broadcasting messages
+        self.counter, _ = await database_sync_to_async(Counter.objects.get_or_create, thread_sensitive=True)(id=1)
+        
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        
         await self.accept()
         await self.send_count_update()
 
     async def disconnect(self, close_code):
-        pass
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # Receive message from WebSocket
     async def receive(self, text_data):
@@ -24,19 +33,39 @@ class CounterConsumer(AsyncWebsocketConsumer):
         elif action == 'decrement':
             await self.decrement_counter()
         
-        await self.send_count_update()
+        await self.send_count_update_broadcast()
 
-    @sync_to_async
+    @database_sync_to_async
     def increment_counter(self):
         self.counter.count += 1
         self.counter.save()
 
-    @sync_to_async
+    @database_sync_to_async
     def decrement_counter(self):
         self.counter.count -= 1
         self.counter.save()
     
     async def send_count_update(self):
+        # Send message to WebSocket client
         await self.send(text_data=json.dumps({
             'count': self.counter.count
+        }))
+
+    async def send_count_update_broadcast(self):
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'count_update',
+                'count': self.counter.count
+            }
+        )
+
+    # Receive message from room group
+    async def count_update(self, event):
+        count = event['count']
+        
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'count': count
         }))
