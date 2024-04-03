4efb83d05989b72a1597c18a88de1998596f7864
Mats <m.w.g.erdkamp@student.tue.nl>
2024-04-03 21:46:07 +0200

    added charge_target_hours

--- digitaltwin/models.py
+++ digitaltwin/models.py
@@ -10,12 +10,13 @@ class Calendar(models.Model):
 
 class ChargeTimeScores(models.Model):
     day = models.DateField(unique=True)
-    scores = models.CharField(validators=[int_list_validator], max_length=200, default='')
+    scores = models.CharField(
+        validators=[int_list_validator], max_length=200, default='')
 
 
 class Car(models.Model):
     # Assuming you want at least the name for the car, you can add more fields as needed
-    
+
     name = models.CharField(max_length=100)
     identifier = models.CharField(max_length=8, default='')
     battery_percentage = (
@@ -31,8 +32,12 @@ class Car(models.Model):
     estimated_time_until_full = (
         models.DurationField()
     )
-    last_update = models.DateTimeField(auto_now=True)  # Automatically updates to current date and time on save
 
+    charge_target_hours =  models.IntegerField(default=5)
+
+
+    # Automatically updates to current date and time on save
+    last_update = models.DateTimeField(auto_now=True)
 
 
 class Counter(models.Model):
