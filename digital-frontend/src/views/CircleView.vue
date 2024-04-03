66ce992ec52ca30afcbad8e7338643830ab33ccc
Mats <m.w.g.erdkamp@student.tue.nl>
2024-04-03 23:56:33 +0200

    swag

--- digital-frontend/src/views/CircleView.vue
+++ digital-frontend/src/views/CircleView.vue
@@ -27,10 +27,10 @@
           Car not plugged in
         </div>
         <div class="charge-indicator" v-else-if="charging == true">
-          Charging {{ batteryPercentage }}
+          Charging {{ batteryPercentage }}%
         </div>
         <div class="charge-indicator" v-else style="background: #e49623">
-          Not Charging {{ batteryPercentage }}
+          Not Charging {{ batteryPercentage }}%
         </div>
       </Transition>
     </div>
